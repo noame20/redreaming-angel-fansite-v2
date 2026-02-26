a231d54cb3fbd75fc017ad1f30cbfb9e33b3f3a4
yui mana <yui@mana.ai>
2026-02-26 08:24:50 +0800

    fix: 使用hash模式适配GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/redreaming-angel-fansite-v2/',
+  build: {
+    assetsDir: 'assets',
+    assetsInlineLimit: 4096,
+  },
 })
